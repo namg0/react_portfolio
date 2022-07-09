ebfd76b724d8720c0d06c6355ca2b748f02296b2
namg0 <yaa2ho@gmail.com>
2022-07-09 23:43:44 +0900

    import js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import './scss/style.scss';
 
+//common
+import './components/common/Header';
+import './components/common/Footer';
+
+//main
+import './components/main/Visual';
+import './components/main/Content';
+
+//sub
+import './components/sub/Department';
+import './components/sub/Gallery';
+import './components/sub/Youtube';
+import './components/sub/Community';
+import './components/sub/Location';
+import './components/sub/Members';
+
 function App() {
 	return <h1>Hello</h1>;
 }
