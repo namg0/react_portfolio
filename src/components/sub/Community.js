302aa76b46dbaf06af7c49d02bc834ef80205205
namg0 <yaa2ho@gmail.com>
2022-07-22 02:30:55 +0900

    community 반응형

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -138,107 +138,112 @@ function Community() {
 		<Layout name={'Community'}>
 			<h1 className='faqH'>FAQ</h1>
 			<div className='FAQ'>
-				<div className='faqArti'>
-					<article>
-						<div className='faqTxt'>
-							<h2>01</h2>
-							<h3>Password search</h3>
-							<p>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea,
-								aperiam omnis. Unde.
-							</p>
-						</div>
-					</article>
-					<article>
-						<div className='faqTxt'>
-							<h2>02</h2>
-							<h3>A lecture refund</h3>
-							<p>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
-								rem ipsum deserunt ipsa sit, officia amet modi nihil incidunt
-								unde.
-							</p>
-						</div>
-					</article>
-					<article>
-						<div className='faqTxt'>
-							<h2>03</h2>
-							<h3>Purchase of supplies</h3>
-							<p>
-								Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque.
-							</p>
-						</div>
-					</article>
-					<article>
-						<div className='faqTxt'>
-							<h2>04</h2>
-							<h3>Leaving the site</h3>
-							<p>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit.
-								Possimus aperiam quae ullam molestias eveniet voluptates eius
-								aut.
-							</p>
-						</div>
-					</article>
+				<div className='inner'>
+					<div className='faqArti'>
+						<article>
+							<div className='faqTxt'>
+								<h2>01</h2>
+								<h3>Password search</h3>
+								<p>
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea,
+									aperiam omnis. Unde.
+								</p>
+							</div>
+						</article>
+						<article>
+							<div className='faqTxt'>
+								<h2>02</h2>
+								<h3>A lecture refund</h3>
+								<p>
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Ratione rem ipsum deserunt ipsa sit, officia amet modi nihil
+									incidunt unde.
+								</p>
+							</div>
+						</article>
+						<article>
+							<div className='faqTxt'>
+								<h2>03</h2>
+								<h3>Purchase of supplies</h3>
+								<p>
+									Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+									Eaque.
+								</p>
+							</div>
+						</article>
+						<article>
+							<div className='faqTxt'>
+								<h2>04</h2>
+								<h3>Leaving the site</h3>
+								<p>
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Possimus aperiam quae ullam molestias eveniet voluptates eius
+									aut.
+								</p>
+							</div>
+						</article>
+					</div>
 				</div>
 			</div>
 
-			<div className='inputBox'>
-				<h2>More Questions</h2>
-				<h1>Our Community</h1>
-				<input type='text' placeholder='Please enter a title' ref={input} />
-				<br />
-				<textarea
-					cols='30'
-					rows='5'
-					placeholder='Please enter the text'
-					ref={textarea}></textarea>
-				<br />
-				<div className='btnSet'>
-					<button onClick={resetForm}>CANCEL</button>
-					<button onClick={createPost}>WRITE</button>
+			<div className='inner'>
+				<div className='inputBox'>
+					<h2>More Questions</h2>
+					<h1>Our Community</h1>
+					<input type='text' placeholder='Please enter a title' ref={input} />
+					<br />
+					<textarea
+						cols='30'
+						rows='5'
+						placeholder='Please enter the text'
+						ref={textarea}></textarea>
+					<br />
+					<div className='btnSet'>
+						<button onClick={resetForm}>CANCEL</button>
+						<button onClick={createPost}>WRITE</button>
+					</div>
 				</div>
-			</div>
-			<div className='showBox'>
-				{Posts.map((post, idx) => {
-					return (
-						<article key={idx}>
-							{post.enableUpdate ? (
-								<>
-									<div className='editTxt'>
-										<input
-											type='text'
-											defaultValue={post.title}
-											ref={inputEdit}
-										/>
-										<br />
-										<textarea
-											cols='30'
-											rows='4'
-											defaultValue={post.content}
-											ref={textareaEdit}></textarea>
-										<br />
-									</div>
-									<div className='btnSet'>
-										<button onClick={() => disableUpdate(idx)}>CANCEL</button>
-										<button onClick={() => updatePost(idx)}>UPDATE</button>
-									</div>
-								</>
-							) : (
-								<>
-									<div className='txt'>
-										<h2>{post.title}</h2>
-										<p>{post.content}</p>
-									</div>
-									<div className='btnSet'>
-										<button onClick={() => enableUpdate(idx)}>EDIT</button>
-										<button onClick={() => deletePost(idx)}>DELELTE</button>
-									</div>
-								</>
-							)}
-						</article>
-					);
-				})}
+				<div className='showBox'>
+					{Posts.map((post, idx) => {
+						return (
+							<article key={idx}>
+								{post.enableUpdate ? (
+									<>
+										<div className='editTxt'>
+											<input
+												type='text'
+												defaultValue={post.title}
+												ref={inputEdit}
+											/>
+											<br />
+											<textarea
+												cols='30'
+												rows='4'
+												defaultValue={post.content}
+												ref={textareaEdit}></textarea>
+											<br />
+										</div>
+										<div className='btnSet'>
+											<button onClick={() => disableUpdate(idx)}>CANCEL</button>
+											<button onClick={() => updatePost(idx)}>UPDATE</button>
+										</div>
+									</>
+								) : (
+									<>
+										<div className='txt'>
+											<h2>{post.title}</h2>
+											<p>{post.content}</p>
+										</div>
+										<div className='btnSet'>
+											<button onClick={() => enableUpdate(idx)}>EDIT</button>
+											<button onClick={() => deletePost(idx)}>DELELTE</button>
+										</div>
+									</>
+								)}
+							</article>
+						);
+					})}
+				</div>
 			</div>
 		</Layout>
 	);
